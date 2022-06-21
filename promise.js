c0b72bc327338afba90e750de7625824c89e3dff
AlgonzaDJun <algonza.20050@mhs.unesa.ac.id>
2022-06-21 23:52:46 +0700

    semoga bisa amin

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const theaterIXX = await promiseTheaterIXX(emosi);
+  const theaterVGC = await promiseTheaterVGC(emosi);
+  
+  if(emosi == 'marah') {
+    return theaterIXX.filter(({hasil}) => hasil == 'marah').length + theaterVGC.filter(({hasil}) => hasil == 'marah').length
+  } else {
+    return theaterIXX.filter(({hasil}) => hasil == 'tidak marah').length + theaterVGC.filter(({hasil}) => hasil == 'tidak marah').length
+  }
+};
 
 module.exports = {
   promiseOutput,
